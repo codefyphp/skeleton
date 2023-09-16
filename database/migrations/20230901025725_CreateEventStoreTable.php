96e293726394329dcd70441ed88c2008b2ff325b
Joshua Parker <joshua@joshuaparker.dev>
2023-09-16 13:12:03 -0600

    Updated migration without prefix and added DB event store implementation.

--- database/migrations/20230901025725_CreateEventStoreTable.php
+++ database/migrations/20230901025725_CreateEventStoreTable.php
@@ -16,24 +16,26 @@ class CreateEventStoreTable extends Migration
      */
     public function up(): void
     {
-        if (!$this->schema()->hasTable(table: env(key: 'DB_TABLE_PREFIX') . 'event_store')) {
+        if (!$this->schema()->hasTable(table: 'event_store')) {
             $this->schema()->create(
-                table: env(key: 'DB_TABLE_PREFIX') . 'event_store',
+                table: 'event_store',
                 callback: function (CreateTable $table) {
                     $table->string(
                         name: 'event_id',
                         length: 36
-                    )->primary()->unique(name: env(key: 'DB_TABLE_PREFIX') . 'event');
+                    )->primary()->unique(name: 'eventId');
+                    $table->string(name: 'transaction_id', length: 36);
                     $table->string(name: 'event_type', length: 191)->notNull();
-                    $table->string(name: 'payload', length: 191);
-                    $table->string(name: 'metadata', length: 191);
-                    $table->string(name: 'aggregate_id', length: 191)->notNull();
+                    $table->string(name: 'event_classname', length: 191)->notNull();
+                    $table->text(name: 'payload')->size(value: 'big')->notNull();
+                    $table->text(name: 'metadata')->size(value: 'big')->notNull();
+                    $table->string(name: 'aggregate_id', length: 36)->notNull();
                     $table->string(name: 'aggregate_type', length: 191)->notNull();
                     $table->integer(name: 'aggregate_playhead')->size(value: 'large')->notNull();
-                    $table->dateTime(name: 'recorded_at');
+                    $table->dateTime(name: 'recorded_at')->notNull();
                     $table->unique(
                         columns: ['aggregate_id','aggregate_type','aggregate_playhead'],
-                        name: env(key: 'DB_TABLE_PREFIX') . 'aggregate'
+                        name: 'domainEvent'
                     );
                 }
             );
@@ -46,8 +48,8 @@ class CreateEventStoreTable extends Migration
      */
     public function down(): void
     {
-        if ($this->schema()->hasTable(table: env(key: 'DB_TABLE_PREFIX') . 'event_store')) {
-            $this->schema()->drop(table: env(key: 'DB_TABLE_PREFIX') . 'event_store');
+        if ($this->schema()->hasTable(table: 'event_store')) {
+            $this->schema()->drop(table: 'event_store');
         }
     }
 }
