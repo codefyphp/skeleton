8a7e9d510bf20f0a303fae7b43111526c165f26e
Joshua Parker <joshua@joshuaparker.dev>
2023-10-11 18:53:57 -0600

    Added config files for cookies and session.

---
+++ config/cookies.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'path' => '/',
+    'domain' => '',
+    'lifetime' => (int) 3600,
+    'secure' => false,
+    'samesite' => 'lax'
+];
---
+++ config/session.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'use_cookies'            => 1,
+    'cookie_secure'          => 1,
+    'cookie_lifetime'        => 3600,
+    'cookie_path'            => '/',
+    'cookie_domain'          => '',
+    'use_only_cookies'       => 1,
+    'cookie_httponly'        => 1,
+    'use_strict_mode'        => 1,
+    'sid_bits_per_character' => 5,
+    'sid_length'             => 48,
+    'cache_limiter'          => 'nocache',
+    'cache_expire'           => 360,
+    'cookie_samesite'        => 'Lax',
+];
