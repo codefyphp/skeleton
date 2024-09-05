22180434de73d7511d118a5cf5cb1aae9cada7e7
Joshua Parker <joshua@joshuaparker.dev>
2024-09-04 20:38:43 -0700

    Updated links for when served with `php codex serve`.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- config/auth.php
+++ config/auth.php
@@ -3,7 +3,7 @@
 use function Codefy\Framework\Helpers\env;
 
 return [
-    'login_url' => env('APP_BASE_URL') . '/public/admin/login/',
+    'login_url' => env('APP_BASE_URL') . '/admin/login/',
 
-    'admin_url' => env('APP_BASE_URL') . '/public/admin/',
+    'admin_url' => env('APP_BASE_URL') . '/admin/',
 ];
