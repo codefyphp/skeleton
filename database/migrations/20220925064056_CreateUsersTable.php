af16a53410f88618106f8d465fcf8d438804a6d6
Joshua Parker <joshua@joshuaparker.dev>
2024-09-04 20:08:43 -0700

    Added CreateUser migration.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- database/migrations/20220925064056_CreateUsersTable.php
+++ database/migrations/20220925064056_CreateUsersTable.php
@@ -20,11 +20,21 @@ class CreateUsersTable extends Migration
                     $table->string(name: 'user_id', length: 36)
                         ->primary()
                         ->unique(name: 'userId');
+                    $table->string(name: 'username', length: 191)
+                        ->unique(name: 'username')
+                        ->notNull();
                     $table->string(name: 'first_name', length: 191);
+                    $table->string(name: 'middle_name', length: 191);
                     $table->string(name: 'last_name', length: 191);
                     $table->string(name: 'email', length: 191)
                         ->unique(name: 'email')
                         ->notNull();
+                    $table->string(name: 'password', length: 191)
+                        ->notNull();
+                    /*$table->string(name: 'confirmation_token', length: 191)
+                        ->unique(name: 'confirmation_token')
+                        ->notNull();*/
+                    $table->dateTime(name: 'created_on');
                 });
         }
     }
