ca0534a8720506554914e72836af7dcebec9d6a5
Joshua Parker <joshua@joshuaparker.dev>
2024-09-26 10:58:32 -0700

    Added generate encryption key command.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ App/Application/Console/Commands/GenerateEncryptionKey.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Console\Commands;
+
+use Codefy\Framework\Application;
+use Codefy\Framework\Console\ConsoleCommand;
+use Defuse\Crypto\Key;
+
+class GenerateEncryptionKey extends ConsoleCommand
+{
+    protected string $name = 'generate:key';
+
+    protected string $description = 'Generates a random encryption key.';
+
+    public function __construct(protected Application $codefy)
+    {
+        parent::__construct(codefy: $codefy);
+    }
+
+    public function handle(): int
+    {
+        $key = Key::createNewRandomKey()->saveToAsciiSafeString();
+
+        $this->terminalRaw(string: sprintf(
+            'Encryption Key: <comment>%s</comment>',
+            $key
+        ));
+
+        // return value is important when using CI
+        // to fail the build when the command fails
+        // 0 = success, other values = fail
+        return ConsoleCommand::SUCCESS;
+    }
+}
