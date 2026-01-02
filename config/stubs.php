1466178cc23fdbd5dd0dd8c151ba0a6c70506663
Joshua Parker <joshua@joshuaparker.dev>
2026-01-01 21:44:11 -0800

    New stub configuration.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ config/stubs.php
@@ -0,0 +1,65 @@
+<?php
+
+use function Codefy\Framework\Helpers\vendor_path;
+
+return [
+    'stubs_path' => vendor_path(path: 'codefyphp/codefy/src/Stubs'),
+    'presets' => [
+        'aggregate' => [
+            'label' => 'Aggregate',
+            'files' => [
+                ['stub' => 'aggregate.stub', 'suffix' => ''],
+            ],
+        ],
+        'event' => [
+            'label' => 'Domain Event',
+            'files' => [
+                ['stub' => 'event.stub', 'suffix' => ''],
+            ],
+        ],
+        'command' => [
+            'label' => 'Command + Handler',
+            'files' => [
+                ['stub' => 'command.stub', 'suffix' => 'Command'],
+                ['stub' => 'command_handler.stub', 'suffix' => 'CommandHandler'],
+            ],
+        ],
+        'query' => [
+            'label' => 'Query + Handler',
+            'files' => [
+                ['stub' => 'query.stub', 'suffix' => 'Query'],
+                ['stub' => 'query_handler.stub', 'suffix' => 'QueryHandler'],
+            ],
+        ],
+        'controller' => [
+            'label' => 'Controller',
+            'files' => [
+                ['stub' => 'controller.stub', 'suffix' => 'Controller'],
+            ],
+        ],
+        'middleware' => [
+            'label' => 'Middleware',
+            'files' => [
+                ['stub' => 'middleware.stub', 'suffix' => 'Middleware'],
+            ],
+        ],
+        'provider' => [
+            'label' => 'Service Provider',
+            'files' => [
+                ['stub' => 'provider.stub', 'suffix' => 'ServiceProvider'],
+            ],
+        ],
+        'console' => [
+            'label' => 'Console Command',
+            'files' => [
+                ['stub' => 'console.stub', 'suffix' => 'Command'],
+            ],
+        ],
+        'repository' => [
+            'label' => 'Aggregate Repository',
+            'files' => [
+                ['stub' => 'repository.stub', 'suffix' => 'AggregateRepository'],
+            ],
+        ],
+    ],
+];
